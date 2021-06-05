5bd6746aff5a5130dc8c13d04ec91322121bb9fe
Luis Heshiki <luisheshiki22@gmail.com>
2021-06-05 17:39:26 +0900

    [update]americanVillageのトップ画を追加

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>American Village</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo get_theme_file_uri('./assets/css/style.css');?>">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_theme_file_uri('./assets/css/style.css');?>">
+</head>
+<body>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <!-- headerナビここくるよ -->
+  <section class="l-content-body">
+    <h1 class="c-topKv__wrapper">
+      <picture>
+        <source srcset="<?php echo get_theme_file_uri('./assets/img/visual_1_pc.png');?>" media='(min-width: 751px)'>
+        <source srcset="<?php echo get_theme_file_uri('./assets/img/visual_1_sp.png');?>" media='(max-width: 750px)'>
+        <img src="<?php echo get_theme_file_uri('./assets/img/visual_1_pc.png');?>" alt="アメリカンビレッジ" class="c-topKv__img">
+      </picture>
+      <div class="c-topKv__logo">
+        <img src="<?php echo get_theme_file_uri('./assets/img/logo.png');?>" alt="">
+      </div>
+      <div class="c-topKv__text">
+        <img src="<?php echo get_theme_file_uri('./assets/img/visual_text_pc.png');?>" alt="">
+      </div>
+      <p class="c-topKv__description">進化し続ける「街」アメリカンビレッジマガジン</p>
+    </h1>
+  </section>
+  <section>
+    <h2>Latest Articles</h2>
+  </section>
+</body>
+</html>
